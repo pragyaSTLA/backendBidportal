6c9bbad2292c60e55c8f0754507cf94e95ef0ede
pragyaSTLA <pragya.dechalwal@stl.tec>
2024-09-11 15:55:16 +0530

    added agp constraint

--- src/main/java/com/portal/bid/controller/AgpController.java
+++ src/main/java/com/portal/bid/controller/AgpController.java
@@ -3,10 +3,14 @@ package com.portal.bid.controller;
 import com.portal.bid.entity.Agp;
 import com.portal.bid.service.AgpService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.Month;
 import java.util.List;
 import java.util.Optional;
 
@@ -40,8 +44,18 @@ public class AgpController {
     @PutMapping("/{id}")
     public ResponseEntity<Agp> updateAgp(@PathVariable Long id, @RequestBody Agp agpDetails) {
         Optional<Agp> agpOptional = agpService.findById(id);
+//        System.out.println(agpOptional+"kdmfsdf");
         if (agpOptional.isPresent()) {
             Agp agp = agpOptional.get();
+//            System.out.println("ispresent");
+            LocalDate financialYearStart = LocalDate.of(LocalDate.now().getYear(), Month.APRIL, 1);
+            LocalDate updateDeadline = financialYearStart.plusMonths(3);
+            if (LocalDate.now().isAfter(updateDeadline)) {
+//                System.out.println("date is agee");
+                HttpHeaders headers = new HttpHeaders();
+                headers.add("Error-Message", "AGP value updates are only allowed in the first three months of the financial year.");
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).headers(headers).body(null);
+            }
             agp.setEmployeeID(agpDetails.getEmployeeID());
             agp.setAccountName(agpDetails.getAccountName());
             agp.setObFY(agpDetails.getObFY());
@@ -51,12 +65,14 @@ public class AgpController {
             agp.setUpdatedBy(agpDetails.getUpdatedBy());
             agp.setStatus(agpDetails.getStatus());
             agp.setUpdatedAt(LocalDateTime.now());
+
             final Agp updatedAgp = agpService.save(agp);
             return ResponseEntity.ok(updatedAgp);
         } else {
             return ResponseEntity.notFound().build();
         }
     }
+
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteAgp(@PathVariable Long id) {
